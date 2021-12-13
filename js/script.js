bb68a5ec451ac4a0bebe3905bb1220cc6341a61c
kangchaelim <kcl75597639@gmail.com>
2021-12-13 15:40:29 +0900

    BORNN

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// description & shop
+// description & shop now
 const button = document.querySelectorAll('body .button');
 console.log(button);
 const shopNow = document.querySelectorAll('body .shop-now');
@@ -24,16 +24,20 @@ for(let k = 0; k<button.length ; k++){
 //   circle
 const circle = document.querySelector('#circle');
 console.log(circle);
+const anamelwareTxt = document.querySelector('#anamelware-txt');
+console.log(anamelwareTxt);
 
-circle.animate(
+anamelwareTxt.addEventListener('mouseenter', ()=>{
+  circle.animate(
     [
        {transform: 'rotate(0)'},
        {transform: 'rotate(360deg)'}
     ],
     {
-        duration :20000,
-        iterations : Infinity
+        duration :20000
     });
+});
+
 
 const circleOne = document.querySelector('#circle1');
 console.log(circleOne);
@@ -69,6 +73,13 @@ console.log(pot);
 const imgBox = document.querySelectorAll('#marbling-imgbox>div');
 console.log(imgBox);
 
+console.log(section[3].children[0]);
+console.log(section[3].children[1]);
+
+// const itemBox = document.querySelectorAll('#items > div');
+// console.log(itemBox);
+
+
 window.addEventListener('scroll', ()=>{
   console.log(scrollY);
 
@@ -95,10 +106,25 @@ window.addEventListener('scroll', ()=>{
       }, (500) * (i+1));
     }
   }
-  if(scrollY >= 3100){
+  if(scrollY >= 2650){
     section[3].style.opacity = 1;
     section[3].style.marginTop = '-10vh';
+    for(let i = 0; i < section[3].children.length; i++){
+      setInterval(()=>{
+        section[3].children[i].classList.replace('item', 'itemRe');
+      }, (1000) * (i+1));
+    }
+
+    // section[3].children[0].classList.remove('item');
+    // section[3].children[1].classList.remove('item');
+
+    // for(let i = 0; i < imgBox.length; i++){
+    //   setInterval(()=>{
+    //     itemBox[i].classList.replace('opa-0', 'opa-1');
+    //   }, (500) * (i+1));
+    // }
   }
+
   if(scrollY >= 3900){
     section[4].style.opacity = 1;
     section[4].style.marginTop = '0';
